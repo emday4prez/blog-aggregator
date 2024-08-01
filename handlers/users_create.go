1ffa92da9c666fb430b9f7bc8a9cebd9bc5a256e
Emerson <emerson.day@gmail.com>
2024-07-31 21:03:51 -0700

    updated mac osx

--- handlers/users_create.go
+++ handlers/users_create.go
@@ -1,3 +1,20 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request){
+	type parameters struct {
+		Name string `json:email`
+	}
+
+	decoder := json.Decoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return 
+	}
+}
