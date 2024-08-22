91c58d74a0081b2c593eed897079f9adf4db6250
Emerson <emerson.day@gmail.com>
2024-08-21 18:49:46 -0700

    db migation 2

--- handler_user_create.go
+++ handler_user_create.go
@@ -34,6 +34,5 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
 		return
 	}
-
 	respondWithJSON(w, http.StatusOK,  databaseUserToUser(user))
 }
