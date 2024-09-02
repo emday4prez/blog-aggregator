255916c7024ea6b1706f3bddb883e26fc6d4b328
Emerson <emerson.day@gmail.com>
2024-09-02 09:16:34 -0700

    get user by api key sql

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,4 +1,7 @@
 -- name: CreateUser :one
 INSERT INTO users (id, created_at, updated_at, name, api_key)
 VALUES ($1, $2, $3, $4, encode(sha256(random()::text::bytea), 'hex'))
-RETURNING id, created_at, updated_at, name, api_key
+RETURNING id, created_at, updated_at, name, api_key;
+
+-- name: GetUserByApiKey :one
+SELECT * FROM users WHERE api_key = $1;
