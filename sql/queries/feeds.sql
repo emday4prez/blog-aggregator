db98c583556ffce57a27e6308eca538b02509bc2
Emerson <emerson.day@gmail.com>
2024-09-15 15:12:12 -0700

    return multiple feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -3,5 +3,5 @@ INSERT INTO feeds (id,created_at,updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4,$5,$6)
 RETURNING id, created_at,updated_at, name, url, user_id;
 
--- name: GetAllFeeds :one
+-- name: GetAllFeeds :many
 SELECT * FROM feeds;
